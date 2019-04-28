1aae4a3c03770e9fceac33475a09de38d516a2ba
万里 一空 <i_write_11konoen@hotmail.co.jp>
2019-04-28 14:25:50 +0900

    バッチファイルの登録

---
+++ .tools/Mbed upgrad.cmd
@@ -0,0 +1,10 @@
+@echo off
+cd ../mbed-os
+python -m pip install --upgrade pip
+mbed sync
+mbed update latest --clean
+mbed releases
+echo *********************************************
+mbed ls
+echo *********************************************
+pause
---
+++ .tools/compile.cmd
@@ -0,0 +1,3 @@
+@echo off
+mbed compile --target NUCLEO_L476RG --toolchain GCC_ARM
+pause
