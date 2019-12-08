6f7e85ba69051132d574399931889bf6057457e3
BanriIkku <i_write_11konoen@hotmail.co.jp>
2019-12-08 18:02:44 +0900

    PCからの入力対応

--- app/_USR_Repetition.cpp
+++ app/_USR_Repetition.cpp
@@ -1,5 +1,9 @@
 #include "../platform/_PLATFORM_main.h"
 
+#include "mbed_trace.h"
+
+#define TRACE_GROUP   "_USR"
+
 
 
 /* ///////////////////////////////////////////////////// */
@@ -9,7 +13,12 @@
  *  詳細説明がここから始まる.
  */
 /* ///////////////////////////////////////////////////// */
-int _USR_Repetition(void)
+int _USR_Repetition(const char *agrs_split, const int len)
 {
+    if (0 < len) {
+        tr_info("%s", agrs_split);
+    }
     return 0;
 }
+
+#undef TRACE_GROUP
