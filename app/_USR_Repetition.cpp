dd1da15a7306f57019aaa1a131db0eb53d340d6a
万里一空 <i_write_11konoen@hotmail.co.jp>
2021-02-27 20:17:58 +0900

    画像データ登録

--- app/_USR_Repetition.cpp
+++ app/_USR_Repetition.cpp
@@ -7,6 +7,17 @@
 E_INK::EPD_Device epd;
 #endif
 #define EPD_SIZE ((400/8)*300)
+
+static char images_data[EPD_SIZE] = {
+#include "data.txt"
+};
+static char images_HertB[EPD_SIZE] = {
+#include "HertF.txt"
+};
+static char images_HertF[EPD_SIZE] = {
+#include "HertB.txt"
+};
+
 /* ///////////////////////////////////////////////////// */
 /*! @brief 要約説明.
  *         要約説明の続き.
@@ -17,20 +28,24 @@ E_INK::EPD_Device epd;
 int _USR_Repetition(const char *agrs_split, const int len)
 {
 #if 1
-    static  char bw_value[EPD_SIZE];
     int bw_length=EPD_SIZE;
+#if 0
+    static  char bw_value[EPD_SIZE];
 
-    static   char red_value[EPD_SIZE];
-    int red_length=EPD_SIZE;
 
     for (int i = 0; i < EPD_SIZE; i++)
     {
         bw_value[i] = 0x0f;
     }
+#endif
+    int red_length=EPD_SIZE;
+#if 1
+    static   char red_value[EPD_SIZE];
     for (int i = 0; i < EPD_SIZE; i++)
     {
-        red_value[i] = 0xAA;
+        red_value[i] = 0x00;
     }
+#endif
 
 
     if (0 < len)
@@ -61,11 +76,24 @@ int _USR_Repetition(const char *agrs_split, const int len)
             case 'c':
                 tr_info("%s", epd.busy()?"YES":"NO");
                 break;
+            case 'h':
+                if (true == epd.Continued())
+                {
+                    tr_info("Refresh");
+                    epd.Refresh(images_HertF, bw_length, images_HertB, red_length);
+                }
+                else
+                {
+                    tr_info("BUSY");
+                }
+
+                /* code */
+                break;
             case 'r':
                 if (true == epd.Continued())
                 {
                     tr_info("Refresh");
-                    epd.Refresh(bw_value, bw_length, red_value, red_length);
+                    epd.Refresh(images_data, bw_length, red_value, red_length);
                 }
                 else
                 {
